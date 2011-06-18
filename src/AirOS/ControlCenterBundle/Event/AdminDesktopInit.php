f3e2e168ae04472b8a986629b6eb7a8d3db83c4e
Matthew Ratzke <matthew.003@me.com>
2011-06-18 15:35:36 -0600

    [AirOSControlCenter] Rewrote desktop events

--- src/AirOS/ControlCenterBundle/Event/AdminDesktopInit.php
+++ src/AirOS/ControlCenterBundle/Event/AdminDesktopInit.php
@@ -3,8 +3,48 @@ namespace AirOS\ControlCenterBundle\Event;
 
 use Symfony\Component\EventDispatcher\Event;
 
-class AdminDesktopInit extends Event
+class InitAdminDesktopEvent extends Event
 {
+	protected $session;
 
+	public function __construct(Session $session)
+	{
+		$this->session = $session;
+
+		if (!$session->has('adminModules'))
+		{
+			$session->set('adminModules', null);
+		}
+	}
+
+	public function setModule($moduleName, $info = array())
+	{
+		//todo: need to check vars to make sure they are of correct type
+		$this->session->set('adminModules', array(
+			'name' => "$moduleName",
+			'info' => $info,
+			)
+		);
+	}
+
+	public function clearModules()
+	{
+		$this->session->set('adminModules', null);
+	}
+
+	//todo: verify this works!!! I hacked it together in 3 minutes, then copied it a bunch
+	public function removeModule($moduleName)
+	{
+		foreach($this->session->getAttributes() as $adminModulesArray => $modulesIndex)
+		{
+			foreach($modulesIndex as $index => $moduleArray)
+			{
+				if($index['name'] == $moduleName)
+				{
+					unset($index);
+				}
+			}
+		}
+	}
 }
 ?>
--- src/AirOS/ControlCenterBundle/Event/UserDesktopInit.php
+++ src/AirOS/ControlCenterBundle/Event/UserDesktopInit.php
@@ -3,8 +3,46 @@ namespace AirOS\ControlCenterBundle\Event;
 
 use Symfony\Component\EventDispatcher\Event;
 
-class UserDesktopInit extends Event
+class InitUserDesktopEvent extends Event
 {
+	protected $session;
 
+	public function __construct(Session $session)
+	{
+		$this->session = $session;
+
+		if (!$session->has('userModules'))
+		{
+			$session->set('userModules', null);
+		}
+	}
+
+	public function setModule($moduleName, $info = array())
+	{
+		//todo: need to check vars to make sure they are of correct type
+		$this->session->set('userModules', array(
+			'name' => "$moduleName",
+			'info' => $info,
+			)
+		);
+	}
+
+	public function clearModules()
+	{
+		$this->session->set('userModules', null);
+	}
+
+	//todo: verify this works!!! I hacked it together in 3 minutes
+	public function removeModule($moduleName)
+	{
+		foreach($this->session->getAttributes() as $userModulesArray => $modulesIndex)
+		{
+			foreach($modulesIndex as $index => $moduleArray)
+				if($index['name'] == $moduleName)
+				{
+					unset($index);
+				}
+		}
+	}
 }
 ?>
